bc8c48aae1cb44397138fba53f09d6af8c63ed82
Xiaoqian Liu (Skylar) <xl3ma@virginia.edu>
2015-06-27 22:29:41 -0500

    Update edit_distance.py

--- edit_distance.py
+++ edit_distance.py
@@ -13,3 +13,17 @@ def minDistance(word1,word2):
             D[x][y] = min(D[x-1][y]+1,D[x][y-1]+1,D[x-1][y-1]+int(word1[x-1]!=word2[y-1]))
     #return D[len(word1)-1][len(word2)-1]
     return D[-1][-1]
+    
+    
+#initialize the matrix only once
+def minDistance1(word1, word2):
+    #initialization
+    if len(word1)==0 or len(word2)==0:
+        return max(len(word1),len(word2))
+    else:
+        D = [[max(y,x) if y==0 or x ==0 else 0 for y in range(0,len(word2)+1)] for x in range(0,len(word1)+1)]        
+        for x in range(1,len(word1)+1):
+            for y in range(1,len(word2)+1):
+                D[x][y] = min(D[x-1][y]+1,D[x][y-1]+1,D[x-1][y-1]+int(word1[x-1]!=word2[y-1]))
+        #return D[len(word1)-1][len(word2)-1]
+        return D[len(word1)][len(word2)]
