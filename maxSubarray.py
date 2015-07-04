b4e5ed21404d582390f248bc06afc00b4d0f6f4e
Xiaoqian Liu <xl3ma@virginia.edu>
2015-07-04 15:07:29 -0500

    Update maxSubarray.py

--- maxSubarray.py
+++ maxSubarray.py
@@ -1,7 +1,7 @@
 def maxSubArray(nums):
     prv = nums[0]
     temp = nums[0]
-    for i in range(1,len(nums)):
+    for i in nums:
         temp+=nums[i]
         #update the max sum if temp sum is greater than max
         prv = max(prv,temp)
