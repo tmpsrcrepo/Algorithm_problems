0bf78e3ebe376728d3551ffe360022152a01eb55
xliu <xl3ma@virginia.edu>
2015-09-08 13:24:53 -0400

    Update findKthLargest.py

--- findKthLargest.py
+++ findKthLargest.py
@@ -1,4 +1,15 @@
 #using partition in quicksort to find kth largest (i.e. (n-k) smallest)
+def partition(nums,l,h):
+    pivot = nums[h]
+    i = l
+    for j in range(l,h):
+        if nums[j]<=pivot:
+            #swap
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+    nums[i],nums[h]=nums[h],nums[i] #next pivot
+    return i
+    
 def findKth(nums,l,h,k):
     if nums:
         pivot_index = partition(nums,l,h)
