73cde30abfb34aa24db99c0a7013a329dc1f638b
xliu <xl3ma@virginia.edu>
2015-09-12 19:11:53 -0400

    Update lengthOfLongestSubstring.py

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,3 +1,4 @@
+#use DP + Hashmap: update the max length when a duplicate is found
 def lengthOfLongestSubstring(s):
     dict_freq = {}
     max_count = 0
@@ -6,6 +7,7 @@ def lengthOfLongestSubstring(s):
     for i in range(len(s)):
         st = s[i]
         if dict_freq.__contains__(st):
+            #update the max_length when encounters a duplicate
             max_count = max(tmp,max_count)
             
             tmp_index = dict_freq.get(st)
