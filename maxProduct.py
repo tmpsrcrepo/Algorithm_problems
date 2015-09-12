7a7ebe9fab8463151b7087403c9a17e0a9fdede9
xliu <xl3ma@virginia.edu>
2015-09-12 19:23:35 -0400

    Update maxProduct.py

--- maxProduct.py
+++ maxProduct.py
@@ -7,7 +7,6 @@ def maxProduct(nums):
     tmp_=1
     for i in nums: 
         if i<0:
-            print 'number',i
             
             if tmp_<0:
                 a=tmp
@@ -17,19 +16,12 @@ def maxProduct(nums):
             else:
                 tmp_=tmp_*i
                 tmp=1
-            print tmp
-            print tmp_
-            print maxP
         if i==0:
             tmp=1
             tmp_=1
             maxP=max(0,maxP)
         if i >0:
-            print 'number',i
             tmp=tmp*i
             tmp_=tmp_*i
             maxP=max(maxP,tmp)
-            print tmp
-            print tmp_
-            print maxP
     return maxP
