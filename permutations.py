02a05993e768f80a98c2c646488ea65a23f8d074
xliu <xl3ma@virginia.edu>
2015-09-16 17:56:41 -0400

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,9 +1,11 @@
 class Permutation_Solution(object):
     def permute_(self,nums,pos,tmp,res):
+        #when there's nothing left, then append the temporary result
         if len(nums)==0:
             res.append(tmp)
         else:
             for i in range(0,len(nums)):
+                #delete an element at a time
                 self.permute_(nums[:i]+nums[i+1:],i,tmp+[nums[i]],res)
     def permute(self, nums):
         res = []
