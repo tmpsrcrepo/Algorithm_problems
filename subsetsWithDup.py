a5588d31a834596f43fd1d711938e280dd2f3af3
xliu <xl3ma@virginia.edu>
2015-09-21 20:22:54 -0400

    Create subsetsWithDup.py

---
+++ subsetsWithDup.py
@@ -0,0 +1,23 @@
+class subsetswithDupSolution(object):
+    def subsetsIterative(self,nums):
+        nums = sorted(nums)
+        result = [[],[nums[0]]]
+        tmpresult = [[nums[0]]]
+        prev = nums[0]
+        if len(nums)>1:
+            for i,n in enumerate(nums[1:]):
+                if prev!=n:
+                    prev = n
+                    tmpresult=[]
+                    for res in result:
+                        tmpresult.append(res+[n])
+                else:
+                    tmpresult = [tmp+[n] for tmp in tmpresult]
+                result+=(tmpresult)
+        return result
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return self.subsetsIterative(nums)
