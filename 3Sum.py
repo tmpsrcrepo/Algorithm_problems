f7e4d08ef0a46596b5374d1027919ccd03c7fede
xliu <xl3ma@virginia.edu>
2015-09-27 15:37:47 -0400

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -4,18 +4,23 @@
         Since a+b+c = 0 and a<=b<=c
         Then there're only two cases: 
         1. a+b < 0, c > 0
+           -> Case 1: a<0, b=0, c=-a
+           -> Case 2: a<0, b>0, c>0
+           -> Case 3: a<0, b<0, c>0
         2. a+b = 0, c = 0,
            however, b can't be greater than 0 since c = 0
-           -> a=0, b=0, c = 0
+           -> Case 4: a=0, b=0, c = 0
         
-        c can only be >=0. dict_pos will store positive candidates
+        c can only be >=0. dict_pos will store positive and zero candidates
         b and c can only be <=0. dict_neg will store negative candidates
         zeros = count(zeros)
         
         After defining the domain of each solution, we traverse the sorted array once to populate dictionaries
-        #if we find 3 zeros, then we append (0,0,0) immediately
-        #then we traverse dict_neg and use the method of solving 2sum (find a,b where a+b = c)
-        
+        if we find 3 zeros, then we append (0,0,0) immediately
+        then we traverse dict_neg and use the method of solving 2sum (find a,b where a+b = c)
+        now target value = -1*current value (=sum of the other two)
+        then we traverse dict_pos and find if any case 1 and 2 can be found
+                 for case 3, we can check its exisitence in dict_neg 
         """
         
 def threeSum(nums):
