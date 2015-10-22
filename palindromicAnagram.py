8d52c1cb1fce504e3ef4107233c0d37fac0c6b9e
xliu <xl3ma@virginia.edu>
2015-10-22 11:54:59 -0400

    Create palindromicAnagram.py

---
+++ palindromicAnagram.py
@@ -0,0 +1,45 @@
+import collections
+import numpy as np
+import math
+
+
+"""
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. An anagram is the result of rearranging the letters of a word or phrase to produce a new word or phrase, using all of the original letters exactly once.
+
+This problem is a combination of both. You will be given a string and your program needs to check whether or not any of its anagrams contain a palindrome. If none of the anagrams is a palindrome, then you need to find the minimum number of characters to eliminate from the string in order for at least one of its anagrams to be a palindrome. In addition, your program needs to figure out the number of anagrams that are palindromes.
+
+Consider this example: You are given the string "aabd". None of the anagrams of this string are palindrome. If you remove the letter "d", however, then "aba" is an anagram of the string that is a palindrome; if you remove the letter "b", then "ada" is an anagram that is a palindrome. In either case, there is only one anagram that is a palindrome.
+"""
+
+class Solution(object):
+    def find(self,str_):
+        uniq = len(collections.Counter(str_).keys())
+        freq = collections.Counter(str_).values()
+        divisible = [i%2 for i in freq]
+        
+        change = np.sum(divisible)-1
+        
+        total = np.sum(freq)
+        fac = 1
+        if change > 0:
+            uniq-=change
+            total -=change
+        count2 = 0
+        for f in freq:
+            if f%2==1:
+                f-=1
+            f = f/2
+            if f>0:
+                fac=(fac*math.factorial(f))
+                count2+=f
+                    #print fac
+        
+        res =math.factorial(count2)/(fac)
+        #res = max(1,res/2)
+        return max(0,change),res
+
+
+s = Solution()
+#print s.find('nnnnjjjnnnjjsss')
+for line in open('PracticeInput_Palindromic.txt','r'):
+    print s.find(line.rstrip())
