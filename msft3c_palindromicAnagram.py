07a364992e3106ad5b3f706890a7faa7133a998a
xliu <xl3ma@virginia.edu>
2015-10-22 11:58:03 -0400

    Update and rename palindromicAnagram.py to msft3c_palindromicAnagram.py

--- msft3c_palindromicAnagram.py
+++ msft3c_palindromicAnagram.py
@@ -4,6 +4,8 @@ import math
 
 
 """
+Microsoft college coding competition question
+
 A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. An anagram is the result of rearranging the letters of a word or phrase to produce a new word or phrase, using all of the original letters exactly once.
 
 This problem is a combination of both. You will be given a string and your program needs to check whether or not any of its anagrams contain a palindrome. If none of the anagrams is a palindrome, then you need to find the minimum number of characters to eliminate from the string in order for at least one of its anagrams to be a palindrome. In addition, your program needs to figure out the number of anagrams that are palindromes.
@@ -24,17 +26,15 @@ class Solution(object):
         if change > 0:
             uniq-=change
             total -=change
-        count2 = 0
         for f in freq:
             if f%2==1:
                 f-=1
             f = f/2
-            if f>0:
+            if f>1:
                 fac=(fac*math.factorial(f))
-                count2+=f
-                    #print fac
+
         
-        res =math.factorial(count2)/(fac)
+        res =math.factorial(total/2)/(fac)
         #res = max(1,res/2)
         return max(0,change),res
 
