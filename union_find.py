6e7e5e158f258817aa2c113c6b1201ad90a576b7
xliu <xl3ma@virginia.edu>
2016-06-05 10:47:38 -0700

    Create union_find.py

---
+++ union_find.py
@@ -0,0 +1,33 @@
+
+class unionFind:
+    def __init__(self,parents):
+        self.parents = parents
+    '''compression find: improve the find cost -> improve union cost
+    '''
+    def find(self,x):
+        while x != self.parents[x]:
+            #update its parent to its grandparents
+            x = self.parents[x] = self.parents[self.parents[x]]
+            #update x
+        return x
+        
+    def find1(self,x):
+        #first find its root
+        fa = parent = self.parents[x]
+        while self.parents[parent] != parent:
+            parent = self.parents[parent]
+        
+        #update predecessor of all the children of the root to the root=parent
+        tmp = 0
+        while self.parents[fa]!= fa:
+            tmp = self.parents[fa]
+            self.parents[fa] = parent
+            fa = tmp
+        return parent
+        
+    
+    def union(self,p,q):
+        p_parent = self.find(p)
+        q_parent = self.find(q)
+        if p_parent != q_parent:
+            self.parents[p_parent] = q_parent
