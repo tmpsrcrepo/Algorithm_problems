7d36f970a049a55339681d51f13b7d686b1a1f6d
xliu <xl3ma@virginia.edu>
2016-06-29 19:28:57 -0700

    Update unique_BST.py

--- unique_BST.py
+++ unique_BST.py
@@ -13,3 +13,17 @@ def numTree1(n):
                 count += (result[itr-i]*result[i-1])
             result.append(count)
         return result[-1]
+
+def numTree(n):
+    dp = [0]*(n+1)
+    if n <= 2:
+        return n
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in xrange(3,n+1):
+        val = 0
+        for j in xrange(1,i+1):
+            val+= max(dp[j-1],1)*max(dp[i-j],1)
+        dp[i]=val
+    return dp[-1]
